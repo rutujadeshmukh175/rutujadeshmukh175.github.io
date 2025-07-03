6a8526451aaca0fcbb7ec7132b46569299582053
Rutuja  Tate Deshmukh <127230985+rutujadeshmukh175@users.noreply.github.com>
2025-07-03 11:37:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const typed = new Typed('.typed', {
+    strings: ['Software Developer', 'Full‑Stack Engineer', 'Problem Solver'],
+    typeSpeed: 60,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true
+  });
+});
